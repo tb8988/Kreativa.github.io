aaef4dbc22bc26ee1cb4ffc990703ee7e011935f
tb8988 <tb8988@users.noreply.github.com>
2025-05-15 22:05:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -491,4 +491,4 @@ function initAnalytics() {
     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
     a.appendChild(r);
   })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-}
+}  
